d8f9ddd53639983700c2d87f86c2f09c90e97115
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-11 13:19:57 +0900

    Initial Commit

---
+++ SwiftUI-Clone App/SwiftUI_Clone_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Clone_AppApp.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Clone_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
