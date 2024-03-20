35f79490c8adc8e99754f3cca8e7bf24ad141fe9
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-20 22:51:04 +0900

    ComplexUI(미완성)

---
+++ SwiftUI-Clone App/ComplexUI/Model/PlayerItem.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerItem.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct PlayerItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerItem()
+}
---
+++ SwiftUI-Clone App/ComplexUI/View/YouTubeHome.swift
@@ -0,0 +1,18 @@
+//
+//  YouTubeHome.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct YouTubeHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTubeHome()
+}
