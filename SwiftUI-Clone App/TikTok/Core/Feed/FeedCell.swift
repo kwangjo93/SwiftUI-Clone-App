4abd0524e38447103375726730274f0213ab3946
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-20 11:01:17 +0900

    FeedCell

---
+++ SwiftUI-Clone App/TikTok/Core/Feed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
