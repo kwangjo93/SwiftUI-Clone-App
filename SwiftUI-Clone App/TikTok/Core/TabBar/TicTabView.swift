bfe7aab9bb191416dbfea3098dae3619e1ae05be
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-20 11:02:15 +0900

    Complete TabView

--- SwiftUI-Clone App/TikTok/Core/TabBar/TicTabView.swift
+++ SwiftUI-Clone App/TikTok/Core/TabBar/TicTabView.swift
@@ -1,5 +1,5 @@
 //
-//  MainTabView.swift
+//  TicTabView.swift
 //  SwiftUI-Clone App
 //
 //  Created by 천광조 on 3/20/24.
@@ -7,14 +7,61 @@
 
 import SwiftUI
 
-struct MainTabView: View {
+struct TicTabView: View {
+    @State private var selectedTab = 0
     var body: some View {
         TabView {
+            FeedView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house" )
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
             
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+            
+            Text("Upload Post")
+                .tabItem { Image(systemName: "plus") }
+            
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
         }
+        .tint(.black)
     }
 }
 
 #Preview {
-    MainTabView()
+    TicTabView()
 }
