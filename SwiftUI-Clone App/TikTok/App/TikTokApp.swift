eb65e4bf77ee44b3aa501b6303f723c32c5ea455
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-20 10:36:47 +0900

    TabView

---
+++ SwiftUI-Clone App/TikTok/App/TikTokApp.swift
@@ -0,0 +1,18 @@
+//
+//  TikTokApp.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct TikTokApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TikTokApp()
+}
---
+++ SwiftUI-Clone App/TikTok/Core/TabBar/TicTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
