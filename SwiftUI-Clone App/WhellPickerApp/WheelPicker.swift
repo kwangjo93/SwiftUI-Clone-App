4ea8726d66d8a8315e410488192e3d35538cfe9c
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-20 16:32:54 +0900

    wheelPicker

---
+++ SwiftUI-Clone App/WhellPickerApp/WheelPicker.swift
@@ -0,0 +1,18 @@
+//
+//  WheelPicker.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/20/24.
+//
+
+import SwiftUI
+
+struct WheelPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WheelPicker()
+}
