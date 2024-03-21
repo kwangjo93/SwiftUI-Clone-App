0a0177b9b2ab67c4c015ed16e5cd8447a5fb7f06
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-21 14:58:08 +0900

    AppleMapBottonSheet

--- SwiftUI-Clone App/AppleMapBottomSheet/Helpers/BottomSheetHelper.swift
+++ SwiftUI-Clone App/AppleMapBottomSheet/Helpers/BottomSheetHelper.swift
@@ -7,12 +7,63 @@
 
 import SwiftUI
 
-struct BottomSheetHelper: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension View {
+    @ViewBuilder
+    func bottomMaskForSheet(mask: Bool = true, _ height: CGFloat = 49) -> some View {
+        self
+            .background(SheetRootViewFinder(mask: mask, height: height))
+    }
+}
+
+fileprivate struct SheetRootViewFinder: UIViewRepresentable {
+    var mask: Bool
+    var height: CGFloat
+    
+    func makeUIView(context: Context) -> some UIView {
+        return .init()
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            if let rootView = uiView.viewBeforeWindow, let window = rootView.window {
+                let safeArea = window.safeAreaInsets
+                rootView.frame = .init(
+                    origin: .zero,
+                    size: .init(
+                        width: window.frame.width,
+                        height: window.frame.height - (mask ? (height + safeArea.bottom) : 0)
+                    )
+                )
+                rootView.clipsToBounds = true
+                for view in rootView.subviews {
+                    view.layer.shadowColor = UIColor.clear.cgColor
+                    
+                    if view.layer.animationKeys() != nil {
+                        if let cornerRadiusView = view.allSubViews.first(where: {
+                            $0.layer.animationKeys()?.contains("cornerRadius") ?? false }) {
+                            cornerRadiusView.layer.maskedCorners = []
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fileprivate extension UIView {
+    //가장 상위의 뷰를 찾는 데 사용됩니다.(UIWindow의 바로 아래 뷰)
+    var viewBeforeWindow: UIView? {
+        if let superview, superview is UIWindow {
+            return self
+        }
+        return superview?.viewBeforeWindow
+    }
+    
+    //Retreiving All Subviews from an UIView
+    var allSubViews: [UIView] {
+        return subviews.flatMap {[$0] + $0.subviews}
     }
 }
 
 #Preview {
-    BottomSheetHelper()
+    AppleMapContentView()
 }
