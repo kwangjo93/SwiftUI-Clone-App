7911dd3183c0548b87921de9d44830b4cdb83c11
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-21 17:46:17 +0900

    StretchySlider

---
+++ SwiftUI-Clone App/StretchySlider/StretchSliderApp.swift
@@ -0,0 +1,18 @@
+//
+//  StretchSliderApp.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/21/24.
+//
+
+import SwiftUI
+
+struct StretchSliderApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StretchSliderApp()
+}
---
+++ SwiftUI-Clone App/StretchySlider/View/CustomSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSlider.swift
+//  SwiftUI-Clone App
+//
+//  Created by 천광조 on 3/21/24.
+//
+
+import SwiftUI
+
+struct CustomSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSlider()
+}
